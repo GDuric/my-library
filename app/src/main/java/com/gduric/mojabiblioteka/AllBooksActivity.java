50d267da18488db0323d30e3f2d21221cff3ef1f
Gordana <g_djuric@hotmail.com>
2021-03-09 10:19:19 +0100

    main activity

---
+++ app/src/main/java/com/gduric/mojabiblioteka/AllBooksActivity.java
@@ -0,0 +1,14 @@
+package com.gduric.mojabiblioteka;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AllBooksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_books);
+    }
+}
--- app/src/main/java/com/gduric/mojabiblioteka/MainActivity.java
+++ app/src/main/java/com/gduric/mojabiblioteka/MainActivity.java
@@ -1,14 +1,26 @@
 package com.gduric.mojabiblioteka;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnAllBooks, btnCurrentlyReading,  btnAlreadyRead, btnToRead,btnFavourites,
+            btnAbout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        btnAllBooks = findViewById(R.id.btnAllBooks);
+        btnCurrentlyReading = findViewById(R.id.btnCurrentlyReading);
+        btnAlreadyRead = findViewById(R.id.btnAlreadyRead);
+        btnToRead = findViewById(R.id.btnToRead);
+        btnFavourites = findViewById(R.id.btnFavourites);
+        btnAbout = findViewById(R.id.btnAbout);
     }
 }
